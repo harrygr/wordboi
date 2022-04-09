70e7a897c5c7a7fa59dc77cab6f3223edcf504b5
Harry Grumbar <harry@grumbar.me>
2022-04-09 11:41:46 +0100

    Allow override of game number

--- src/useSolution.ts
+++ src/useSolution.ts
@@ -1,11 +1,22 @@
+import { useRouter } from "next/router";
 import React from "react";
 import { gameConfig } from "./GameState";
 import { diffInDays } from "./utils";
 import { words } from "./wordList";
 
 export const useSolution = () => {
-  const now = React.useMemo(() => new Date(), []);
-  const gameNumber = diffInDays(now, gameConfig.firstDay);
+  const router = useRouter();
+
+  // derive the game number from the `n` querystring.
+  // otherwise get today's game number
+  const gameNumber = React.useMemo(() => {
+    const n = parseInt(`${router.query.n}`, 10);
+
+    return typeof n === "number" && !isNaN(n)
+      ? n
+      : diffInDays(new Date(), gameConfig.firstDay);
+  }, [router.query.n]);
+
   const solution = React.useMemo(() => words[gameNumber], [gameNumber]);
 
   return { gameNumber, solution };
