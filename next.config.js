71b1e58bb4b82e4b4736b2128b38f1aa6320af2b
Harry Grumbar <harry@grumbar.me>
2022-08-16 19:26:32 +0100

    Add PWA config and icons

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+  },
+});
+
+module.exports = nextConfig;
