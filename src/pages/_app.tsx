4fc70920d4d816481e7d614940d05b5f091afc9e
Harry Grumbar <harry@grumbar.me>
2022-08-22 16:34:41 +0100

    Add viewport meta tag to prevent accidental zoom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
         <script
           async
           defer
